86988aaa2502cbe8ed780ceeb9147e4595318ae1
Joshua Washington <jdwashington195@gmail.com>
2025-01-28 15:40:46 -0500

    MainActivity.kt
    Update Main Activity

--- FPGroup/app/src/main/java/com/example/fpgroup/MainActivity.kt
+++ FPGroup/app/src/main/java/com/example/fpgroup/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fpgroup
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -8,6 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userEmail = intent.getStringExtra("USER_EMAIL")
+
     }
 
 }
