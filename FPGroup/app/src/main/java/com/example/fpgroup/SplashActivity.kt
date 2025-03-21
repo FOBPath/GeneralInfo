9dacdab8409789bc06e0aa7b43d6823e76ad134e
Joshua Washington <jdwashington195@gmail.com>
2025-03-21 13:51:14 -0400

    Update SplashActivity.kt

--- FPGroup/app/src/main/java/com/example/fpgroup/SplashActivity.kt
+++ FPGroup/app/src/main/java/com/example/fpgroup/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.example.fpgroup
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -10,10 +11,12 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000) // 2-second delay
+        }, 2000)
+
+
     }
 }
