5a637ae4fea79ec431ce03e46e8cf77f25d9f7ec
Joshua Washington <jdwashington195@gmail.com>
2025-03-31 19:59:11 -0400

    Update build.gradle.kts

--- FPGroup/build.gradle.kts
+++ FPGroup/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false // 
+    id("com.google.gms.google-services") version "4.3.15" apply false //
 }
 
 allprojects {
